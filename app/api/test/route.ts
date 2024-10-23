8115828f8f5eb99e314526920019d21037f1f8f8
mijip0320 <39548731+mijip0320@users.noreply.github.com>
2024-10-24 07:41:03 +0900

    boilerplate 업데이트

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -3,10 +3,11 @@ import { NextRequest, NextResponse } from "next/server";
 const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
 export async function GET(request: NextRequest) {
+  const randomDelay = Math.floor(Math.random() * 5000);
+  console.log("randomDelay", randomDelay);
 
-    const randomDelay = Math.floor(Math.random() * 5000);
-    console.log("randomDelay", randomDelay);
+  throw new Error();
 
-    await delay(randomDelay);
-    return NextResponse.json({data : `${randomDelay}만에 응답 완료`})
+  await delay(randomDelay);
+  return NextResponse.json({ data: `${randomDelay}만에 응답 완료` });
 }
